35256ad602a76b89492d4397dd73408408eef9f7
github username <github email address>
2019-09-11 15:07:09 -0600

    add review prompts

--- runner.rb
+++ runner.rb
@@ -6,5 +6,9 @@ cli = Cli.new
 beer_name = cli.prompt_for_beer_name
 beer_info = Beer.get_beer_info_by_name(beer_name)
 cli.display_beer_info(beer_info)
+review_hash = cli.prompt_for_review
+new_review = Review.new(review_hash)
 
 binding.pry
+
+
