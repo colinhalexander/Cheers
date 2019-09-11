ae50c0128611b369b8bde1bb44c0679b0f70eee2
colinhalexander <chalexander@icloud.com>
2019-09-11 16:32:34 -0600

    finished beer display and review function

--- runner.rb
+++ runner.rb
@@ -1,14 +1,22 @@
 require_relative './config/environment'
 
-current_user = User.new(name: "Randy")
-cli = Cli.new
+def start
+    current_user = User.create(name: "Randy")
+    cli = Cli.new
 
-beer_name = cli.prompt_for_beer_name
-beer_info = Beer.get_beer_info_by_name(beer_name)
-cli.display_beer_info(beer_info)
-review_hash = cli.prompt_for_review
-new_review = Review.new(review_hash)
+    beer_name = cli.prompt_for_beer_name
+    beer_info = Beer.get_beer_info_by_name(beer_name)
+    cli.display_beer_info(beer_info)
 
-binding.pry
+    review_hash = cli.prompt_for_review
+    review_hash[:user_id] = current_user.id
+    review_hash[:beer_id] = Beer.find_by(name: beer_name).id
+    new_review = Review.create(review_hash)
+    start
+end
+
+start
+
+# binding.pry
 
 
